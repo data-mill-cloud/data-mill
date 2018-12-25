c06277352e11479178c4982010b1ca33e54cfbf1
pilillo <pilillo@ryzen>
2018-12-25 15:52:48 +0100

    merry christmas

--- infrastructure/components/influxdb/setup.sh
+++ infrastructure/components/influxdb/setup.sh
@@ -11,9 +11,9 @@ if [ -z "$1" ] || [ "$1" != "install" ] && [ "$1" != "delete" ];then
 	exit 1
 elif [ "$1" = "install" ]; then
 	# https://github.com/helm/charts/tree/master/stable/superset
-	helm upgrade --install $cfg__superset__release \
+	helm upgrade --install $cfg__influxdb__release \
 	 --namespace $cfg__project__k8s_namespace \
-	 -f $file_folder/$cfg__superset__config_file stable/superset
+	 -f $file_folder/$cfg__influxdb__config_file stable/influxdb
 else
-	helm delete $cfg__superset__release --purge
+	helm delete $cfg__influxdb__release --purge
 fi
