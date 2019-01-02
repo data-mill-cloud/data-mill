2f55b34ad5943741b20369f6912c8fbe2e2bfb4b
pilillo <pilillo@ryzen>
2019-01-02 01:14:01 +0100

    minor changes

--- infrastructure/components/kafka/setup.sh
+++ infrastructure/components/kafka/setup.sh
@@ -15,16 +15,15 @@ if [ -z "$ACTION" ] || [ "$ACTION" != "install" ] && [ "$ACTION" != "delete" ];t
         echo "usage: $0 {'install' | 'delete'}";
         exit 1
 elif [ "$ACTION" = "install" ]; then
-
 	# https://strimzi.io/quickstarts/minikube/
-	helm repo add strimzi http://strimzi.io/charts/ 
-    helm install strimzi/strimzi-kafka-operator --name $cfg__kafka__release --namespace $cfg__project__k8s_namespace
+	helm repo add strimzi http://strimzi.io/charts/
+	helm install strimzi/strimzi-kafka-operator --name $cfg__kafka__release --namespace $cfg__project__k8s_namespace
 	latest_strimzi=$(get_latest_github_release "strimzi/strimzi-kafka-operator")
 	kubectl apply -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/$latest_strimzi/examples/kafka/kafka-persistent.yaml \
 	 --namespace $cfg__project__k8s_namespace
 
 else
-    helm delete --purge $cfg__kafka__release
+	helm delete --purge $cfg__kafka__release
 	kubectl delete -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/$latest_strimzi/examples/kafka/kafka-persistent.yaml \
 	 --namespace $cfg__project__k8s_namespace
 fi
