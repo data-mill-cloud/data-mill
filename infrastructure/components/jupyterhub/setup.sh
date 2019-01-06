48d624b8e0ef40df99bcfc930dac4ac1881caf31
pilillo <pilillo@ryzen>
2019-01-06 18:33:42 +0100

    added datamill DS environments

--- infrastructure/components/jupyterhub/setup.sh
+++ infrastructure/components/jupyterhub/setup.sh
@@ -14,17 +14,6 @@ if [ -z "$ACTION" ] || [ "$ACTION" != "install" ] && [ "$ACTION" != "delete" ];t
         echo "usage: $0 {'install' | 'delete'}";
         exit 1
 elif [ "$ACTION" = "install" ]; then
-	# build the datascience image
-	#if [ "$cfg__jhub__ds_image__use_local_image" = true ];then
-	#	folder_name=`basename $cfg__jhub__ds_image__name`
-	#	echo "Building $cfg__jhub__ds_image__name:$cfg__jhub__ds_image__tag from $file_folder/ds_environments/$folder_name/Dockerfile"
-	#	eval $(minikube docker-env)
-	#	docker build -t $cfg__jhub__ds_image__name:$cfg__jhub__ds_image__tag -f $file_folder/ds_environments/$folder_name/Dockerfile .
-	#	unset folder_name
-	#else
-	#	echo "Using community image $cfg__jhub__ds_image__name:$cfg__jhub__ds_image__tag"
-	#fi
-
 	# following https://z2jh.jupyter.org/en/stable/setup-jupyterhub.html
 	helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 	helm repo update
@@ -36,7 +25,6 @@ elif [ "$ACTION" = "install" ]; then
 	  --values $file_folder/$cfg__jhub__config_file \
 	  --set proxy.secretToken=$secretToken \
 	  --install --force --timeout $cfg__jhub__setup_timeout --wait
-	#--set proxy.secretToken=$secretToken,singleuser.image.name=$cfg__jhub__ds_image__name,singleuser.image.tag=$cfg__jhub__ds_image__tag \
 	unset secretToken
 else
 	helm delete $cfg__jhub__release --purge
