b04c11fe94078b947ac4b9244ddbc1a6bd0ba21f
pilillo <pilillo@ryzen>
2019-01-09 18:42:40 +0100

    added build and push for mlflow image

---
+++ infrastructure/helm-charts/mlflow/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+t=datamillcloud/mlflow
+version=0.1
+
+docker login
+
+docker build -t $t:$version -f Dockerfile .
+commit_tag=$(docker images $t:$version | awk 'FNR > 1{ print $3 }')
+echo "Commit tag: $commit_tag"
+
+echo "-> docker tag $commit_tag $t:$version"
+docker tag $commit_tag $t:$version
+echo "-> docker push $t"
+docker push $t
+
+echo "-> docker tag $commit_tag $t:latest"
+docker tag $commit_tag $t:latest
+echo "-> docker push $t"
+docker push $t
