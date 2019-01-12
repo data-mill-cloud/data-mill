9307613685347397d320463615ac62e7be8ee8fc
o6323159 <o6323159@nwytg.net>
2019-01-12 15:50:30 +0100

    added setup

--- infrastructure/k8s/setup.sh
+++ infrastructure/k8s/setup.sh
@@ -49,14 +49,18 @@ elif [ "$LOCATION" = "local" ]; then
 			echo  "latest minikube version available is $latest_minikube"
 			curl -Lo minikube https://storage.googleapis.com/minikube/releases/$latest_minikube/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
         	}
-	elif [ "$cfg__local__provider" = "mikrok8s" ]; then
-		echo "Setting up Mikrok8s cluster"
+	elif [ "$cfg__local__provider" = "microk8s" ]; then
+		echo "Setting up Microk8s cluster"
 		command -v snap >/dev/null 2>&1 || {
 			echo "snap package manager is missing and required to install mikrok8s! Exiting.."
 			exit 1
 		}
-		echo "ok snap is there"
-		exit 1
+		echo "ok, snap is there"
+		echo "checking microk8s installation"
+		snap list microk8s || {
+			echo "microk8s is missing, trying to install it now"
+			snap install microk8s --classic
+		}
 	else
 		echo "Local K8s provider $cfg__local__provider not supported!"
 	fi
@@ -71,15 +75,13 @@ elif [ "$LOCATION" = "local" ]; then
 		chmod +x ./kubectl
 		mv ./kubectl /usr/local/bin/kubectl
 	}
-
 	if [ "$cfg__local__provider" = "minikube" ]; then
 		# starting minikube cluster if not already started (to make sure this whole script is idempotent)
 		kb_status=$(minikube status | grep "host:" | awk '{print $2}' FS=': ')
 		if [ -z "$kb_status" ] || [ "$kb_status" = "Stopped" ]; then
 			# starting minikube
-			echo "Starting minikube.."
+			echo "starting local k8s provider $cfg__local__provider"
 			echo "minikube start --cpus $cfg__local__cpus --memory $cfg__local__memory --disk-size=$cfg__local__storage --vm-driver $cfg__local__vm_driver "$( ( "$cfg__local__gpu_support" = true ) && printf %s '--gpu' )
-			cfg__local__mnt_data="/mnt/vda1/data/storage/"
 			# in case of issues with mounting, it may be due to the vm driver (they behave differently) or more probably to a firewall issue on the host
 			# https://github.com/kubernetes/minikube/issues/2379
 			# https://github.com/kubernetes/minikube/issues/1548
@@ -101,9 +103,6 @@ elif [ "$LOCATION" = "local" ]; then
 			# GPU setup explained at https://github.com/kubernetes/minikube/blob/master/docs/gpu.md
 
 			echo "Minikube VM started. Node accessible using 'minikube ssh'"
-			echo "Creating data dir $cfg__local__mnt_data"
-			minikube ssh "sudo mkdir -p $cfg__local__mnt_data"
-			minikube ssh "sudo chown rkt:rkt $cfg__local__mnt_data"
 
 			# enable add-ons
 			# https://github.com/kubernetes/minikube/blob/master/docs/addons.md
@@ -117,11 +116,31 @@ elif [ "$LOCATION" = "local" ]; then
 		else
 			echo "Minikube is already running. Enjoy!"
 		fi
-	elif [ "$cfg__local__provider" = "mikrok8s" ]; then
-		echo "Starting mikrok8s cluster"
-        else
-                echo "Local K8s provider $cfg__local__provider not supported!"
-        fi
+	elif [ "$cfg__local__provider" = "microk8s" ]; then
+	    echo "checking install"
+		microk8s.status
+		status=$?
+		if [ $status -eq 1 ]; then
+			echo "starting local k8s provider $cfg__local__provider"
+			microk8s.start
+			microk8s.status --wait-ready --timeout 120
+			microk8s.enable dns registry storage
+		elif [ $status -eq 0 ]; then
+			echo "Local K8s provider $cfg__local__provider is already running"
+		else
+			echo "cannot check the status of local k8s provider $cfg__local__provider, Exiting!"
+			exit 1
+		fi
+		if [ ! -z "$cfg__local__gpu_support" ] && [ "$cfg__local__gpu_support" = true ]; then
+			microk8s.enable gpu
+		fi
+		# switch context
+		kubectl config use-context $cfg__local__provider
+		# create a namespace for us
+		kubectl create namespace $cfg__project__k8s_namespace
+	else
+		echo "Local K8s provider $cfg__local__provider not supported!"
+	fi
 
 	# add an overlay network if required
 	if [ ! -z "$cfg__local__use_overlay" ] && [ "$cfg__local__use_overlay" = true ]; then
