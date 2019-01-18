dd3c22f40c5aca3a5003c5c9cc41cc0d2121d700
pilillo <pilillo@ryzen>
2019-01-18 19:40:52 +0100

    removed debug echoes from minio and pachyderm

--- infrastructure/components/minio/setup.sh
+++ infrastructure/components/minio/setup.sh
@@ -18,16 +18,18 @@ elif [ "$ACTION" = "install" ]; then
 	# deploying minio to k8s
 	random_key=$(get_random_string_key 32)
 	random_secret=$(get_random_secret_key)
-	echo "Starting Minio with:"
-	echo "- KEY:$random_key"
-	echo "- SECRET:$random_secret"
+	# debug only, to be removed
+	#echo "Starting Minio with:"
+	#echo "- KEY:$random_key"
+	#echo "- SECRET:$random_secret"
 
 	# https://github.com/helm/charts/tree/master/stable/minio#configuration
 	helm upgrade $cfg__minio__release stable/minio \
 	 --namespace $cfg__project__k8s_namespace \
 	 --values $file_folder/$cfg__minio__config_file \
-	 --set accessKey=$random_key,secretKey=$random_secret,persistence.storageClass=$cfg__minio__storageClass \
-	 --install --force
+	 --set accessKey=$random_key,secretKey=$random_secret,persistence.storageClass=$cfg__minio__storage_class \
+	 --recreate-pods \
+	 --install --force $( [ ! -z $cfg__minio__setup_timeout ] && [ $cfg__minio__setup_timeout -gt 0 ] && printf %s "--timeout $cfg__minio__setup_timeout --wait" )
 	unset random_key
 	unset random_secret
 
--- infrastructure/components/pachyderm/setup.sh
+++ infrastructure/components/pachyderm/setup.sh
@@ -15,7 +15,6 @@ if [ -z "$ACTION" ] || [ "$ACTION" != "install" ] && [ "$ACTION" != "delete" ];t
         echo "usage: $0 {'install' | 'delete'}";
         exit 1
 elif [ "$ACTION" = "install" ]; then
-	echo "Pachyderm set to use dalake $cfg__pachyderm__datalake__type"
 	if [ "$cfg__pachyderm__datalake__type" = "minio" ]; then
 		ACCESS_KEY=$(kubectl -n $cfg__project__k8s_namespace get secrets $cfg__pachyderm__datalake__minio_release -o jsonpath="{.data.accesskey}" | base64 -d)
 		SECRET_KEY=$(kubectl -n $cfg__project__k8s_namespace get secrets $cfg__pachyderm__datalake__minio_release -o jsonpath="{.data.secretkey}" | base64 -d)
@@ -24,6 +23,7 @@ elif [ "$ACTION" = "install" ]; then
 		# before installing pachyderm, make sure a bucket named $cfg__pachyderm__bucket is available on minio
 		#echo $(datalake_run_command "$cfg__project__k8s_namespace" "mc config host add minio http://$cfg__pachyderm__datalake__minio_release:9000 $ACCESS_KEY $SECRET_KEY --api S3v4 && mc mb minio/$cfg__pachyderm__datalake__bucket --ignore-existing")
 		$(minio_create_bucket $cfg__project__k8s_namespace $cfg__pachyderm__datalake__minio_release $cfg__project__data_folder $cfg__pachyderm__datalake__bucket)
+		echo "Set minio bucket $cfg__pachyderm__datalake__bucket"
 	else
 		ACCESS_KEY=$cfg__pachyderm__datalake__access_key
 		SECRET_KEY=$cfg__pachyderm__datalake__secret_key
@@ -32,12 +32,15 @@ elif [ "$ACTION" = "install" ]; then
 		echo $(datalake_run_command "$cfg__project__k8s_namespace" "mc config host add dl $ENDPOINT $ACCESS_KEY $SECRET_KEY --api S3v4 && mc mb dl/$cfg__pachyderm__datalake__bucket --ignore-existing")
 	fi
 
+	echo "Pachyderm set to use dalake $cfg__pachyderm__datalake__type"
+
 	# https://hub.helm.sh/charts/stable/pachyderm
 	helm upgrade $cfg__pachyderm__release stable/pachyderm \
 	 --version $cfg__pachyderm__version \
 	 --namespace $cfg__project__k8s_namespace \
 	 --values $file_folder/$cfg__pachyderm__config_file \
 	 --set s3.accessKey=$ACCESS_KEY,s3.secretKey=$SECRET_KEY,s3.bucketName=$cfg__pachyderm__datalake__bucket,s3.endpoint=$ENDPOINT \
+	 --recreate-pods \
 	 --install --force
 else
 	helm delete $cfg__pachyderm__release --purge
