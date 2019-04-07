c9c1d69774ca877a78e4060bdaf1392230f4fa08
pilillo <pilillo@ryzen.local>
2019-04-07 18:37:17 +0200

    edited prometheus setup to delete CRDs upon uninstall

--- infrastructure/components/monitoring-stack/setup.sh
+++ infrastructure/components/monitoring-stack/setup.sh
@@ -8,6 +8,8 @@ eval $(get_component_config)
 # use if set or a string argument otherwise
 ACTION=${ACTION:=$1}
 
+PROMETHEUS_BASE_PATH="https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd"
+
 if [ -z "$ACTION" ] || [ "$ACTION" != "install" ] && [ "$ACTION" != "delete" ];then
         echo "usage: $0 {'install' | 'delete'}";
         exit 1
@@ -15,10 +17,10 @@ elif [ "$ACTION" = "install" ]; then
 	helm repo update
 
 	# adding prometheus operator crd's
-	kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
-	kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
-	kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
-	kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
+	kubectl apply -f ${PROMETHEUS_BASE_PATH}/alertmanager.crd.yaml
+	kubectl apply -f ${PROMETHEUS_BASE_PATH}/prometheus.crd.yaml
+	kubectl apply -f ${PROMETHEUS_BASE_PATH}/prometheusrule.crd.yaml
+	kubectl apply -f ${PROMETHEUS_BASE_PATH}/servicemonitor.crd.yaml
 
 	random_secret=$(get_random_secret_key)
 
@@ -33,4 +35,10 @@ elif [ "$ACTION" = "install" ]; then
 	unset random_secret
 else
 	helm delete $cfg__monitoring__release --purge
+
+	# delete CRDs
+        kubectl delete -f ${PROMETHEUS_BASE_PATH}/alertmanager.crd.yaml
+        kubectl delete -f ${PROMETHEUS_BASE_PATH}/prometheus.crd.yaml
+        kubectl delete -f ${PROMETHEUS_BASE_PATH}/prometheusrule.crd.yaml
+        kubectl delete -f ${PROMETHEUS_BASE_PATH}/servicemonitor.crd.yaml
 fi
