4890f280b9fddea400efb9a0b9c40aff61692144
Andrea Monacchi <andrea.monacchi@gmail.com>
2019-07-25 19:04:13 +0200

    switched from mlflow ui to tracking server (testing)

--- infrastructure/helm-charts/mlflow/build.sh
+++ infrastructure/helm-charts/mlflow/build.sh
@@ -5,7 +5,8 @@ version=0.1
 
 docker login
 
-docker build --build-arg ml_folder=/mnt/mlflow_data -t $t:$version -f Dockerfile .
+# the build arguments are then passes as folder and artifact_folder ENV variables, which can be therefore overwritten with -e folder=whatever
+docker build --build-arg ml_folder=/mnt/mlflow_data --build-arg ml_artifact_folder=/mnt/mlflow_data -t $t:$version -f Dockerfile .
 commit_tag=$(docker images $t:$version | awk 'FNR > 1{ print $3 }')
 echo "Commit tag: $commit_tag"
 
