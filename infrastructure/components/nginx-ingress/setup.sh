18859d2f9c2ac3f254dbe1d0770ac8d5b9975c7d
Andrea Monacchi <andrea.monacchi@gmail.com>
2019-07-30 15:48:34 +0200

    added nginx-ingress as component

---
+++ infrastructure/components/nginx-ingress/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# load component paths
+eval $(get_paths)
+# load local yaml config
+eval $(get_component_config)
+
+# use if set or a string argument otherwise
+ACTION=${ACTION:=$1}
+
+if [ -z "$ACTION" ] || [ "$ACTION" != "install" ] && [ "$ACTION" != "delete" ];then
+        echo "usage: $0 {'install' | 'delete'}";
+        exit 1
+elif [ "$ACTION" = "install" ]; then
+	# https://kubernetes.github.io/ingress-nginx/deploy/baremetal/
+	helm upgrade $cfg__nginx_ingress__release stable/nginx-ingress \
+	 --namespace $cfg__project__k8s_namespace \
+	 --values $(get_values_file "$cfg__nginx_ingress__config_file") \
+	 --install --force
+else
+	helm delete $cfg__nginx_ingress__release --purge
+fi
