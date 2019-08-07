40d704dddd5f65bc67407143bd8141feab3d02f7
pilillo <>
2019-08-07 16:32:58 +0200

    edited mlflow helm

--- infrastructure/helm-charts/mlflow/build.sh
+++ infrastructure/helm-charts/mlflow/build.sh
@@ -6,7 +6,13 @@ version=0.1
 docker login
 
 # the build arguments are then passes as folder and artifact_folder ENV variables, which can be therefore overwritten with -e folder=whatever
-docker build --build-arg ml_folder=/mnt/mlflow_data --build-arg ml_artifact_folder=/mnt/mlflow_data -t $t:$version -f Dockerfile .
+
+docker build \
+$( [ ! -z $http_proxy ] && echo "--build-arg proxy=$http_proxy" ) --build-arg ml_folder=/mnt/mlflow_data --build-arg ml_artifact_folder=/mnt/mlflow_data \
+-t $t:$version -f Dockerfile .
+
+#docker build --build-arg ml_folder=/mnt/mlflow_data --build-arg ml_artifact_folder=/mnt/mlflow_data -t $t:$version -f Dockerfile .
+
 commit_tag=$(docker images $t:$version | awk 'FNR > 1{ print $3 }')
 echo "Commit tag: $commit_tag"
 
