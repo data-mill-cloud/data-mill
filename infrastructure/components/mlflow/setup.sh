641e41de0ef8a84dbbf171766231d74b018d0a83
pilillo <>
2019-08-22 17:08:08 +0200

    mlflow

--- infrastructure/components/mlflow/setup.sh
+++ infrastructure/components/mlflow/setup.sh
@@ -12,11 +12,6 @@ if [ -z "$ACTION" ] || [ "$ACTION" != "install" ] && [ "$ACTION" != "delete" ];t
         echo "usage: $0 {'install' | 'delete'}";
         exit 1
 elif [ "$ACTION" = "install" ]; then
-
-        # creating the PVC for mlflow
-        $(find_by_volume_name_and_create $cfg__project__k8s_namespace "$file_folder/volumes/pvc/*" $cfg__mlflow__pvc_name "pvc")
-        kubectl get pvc $cfg__mlflow__pvc_name -n=$cfg__project__k8s_namespace
-
 	# check if the artifact store is supposed to use a local S3 or AWS S3
 	if [ "$cfg__mlflow__artifact_store__type" = "minio" ]; then
                 ACCESS_KEY=$(kubectl -n $cfg__project__k8s_namespace get secrets $cfg__mlflow__artifact_store__minio_release -o jsonpath="{.data.accesskey}" | base64 -d)
@@ -35,7 +30,6 @@ elif [ "$ACTION" = "install" ]; then
 
         echo "Mlflow set to use artifact store $cfg__mlflow__artifact_store__type"
 
-
 	# adding and updating repo and helm chart
 	helm repo add data-mill https://data-mill-cloud.github.io/data-mill/helm-charts/
 	helm repo update
@@ -43,7 +37,7 @@ elif [ "$ACTION" = "install" ]; then
 	helm upgrade $cfg__mlflow__release data-mill/mlflow \
          --namespace $cfg__project__k8s_namespace \
          --values $(get_values_file "$cfg__mlflow__config_file") \
-	 --set server.artifacturi="s3://:5000/mlflow" \
+	 --set server.artifacturi="s3://mlflow",server.s3endpoint="http://${cfg__mlflow__artifact_store__minio_release}:9000",server.accesskey="${ACCESS_KEY}",server.secretkey="${SECRET_KEY}" \
          --install --force
 else
         helm delete $cfg__mlflow__release --purge
